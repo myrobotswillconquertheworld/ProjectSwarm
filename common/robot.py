350aee6e2d2c02c405748d83dc398b8f7743857e
John Karantez <myrobotswillconquertheworld@gmail.com>
2019-07-31 22:10:33 +0200

    mbf

--- common/robot.py
+++ common/robot.py
@@ -125,8 +125,9 @@ class Robot:
         with open('common/config.yaml') as f:
             swarm_config = yaml.load(f, Loader=yaml.FullLoader)
             #logging.info("loading config for:" % self.robot_name)
-            print(swarm_config)
-            self.robot_config = swarm_config[self.robot_name]
+            print(swarm_config[0])
+            self.robot_config = swarm_config[0]
+            self.robot_config = self.robot_config[self.robot_name] 
         
         return True
     
